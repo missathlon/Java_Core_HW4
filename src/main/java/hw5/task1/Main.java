8e3f30455d89e88cb3cb3a87a44090e38d05beb6
Ekaterina <missathlon08@gmail.com>
2024-11-20 16:48:07 +0300

    hw5

---
+++ src/main/java/hw5/task1/Main.java
@@ -0,0 +1,29 @@
+package hw5.task1;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.newDirectoryStream;
+
+/*
+Написать функцию, создающую резервную копию всех файлов в директории(без поддиректорий) во вновь созданную папку ./backup
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Files.createDirectory(Path.of("./backup"));
+            DirectoryStream<Path> directoryStream = newDirectoryStream(Path.of("."));
+            for (Path file : directoryStream) {
+                if (Files.isDirectory(file)) {
+                    continue;
+                }
+                Files.copy(file, Path.of("./backup/" + file.toString()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
